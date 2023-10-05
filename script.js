ba5c09224798cef76d09a36ea4c2eabd7c530a3b
BG00000 <abakergabb@gmail.com>
2023-10-05 11:01:14 +1100

    added more styling, added function to create btns, added btn interactivity

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-const numButtons = document.querySelectorAll('#container div button')
+document.addEventListener('DOMContentLoaded', () => {
 
-numButtons.forEach((button) => {
-    let mouseDown
-    button.addEventListener('mouseup', () => {
-        mouseDown = false
-        button.style.borderLeft = '2px solid rgb(160, 160, 160)'
-        button.style.borderBottom = '2px solid rgb(160, 160, 160)'
+
+    const numbersDiv = document.querySelector('#numbersDiv')
+    const outputField = document.querySelector('#output') 
+
+    let output = ''
+
+    for(i = 0; i < 10; i++) {
+        const btn = document.createElement('button')
+        btn.innerText = i
+        numbersDiv.appendChild(btn)
+
+        btn.addEventListener('click', () => {
+            output += btn.innerText
+            outputField.innerText = output
+        })
+    }
+
+    const numButtons = document.querySelectorAll('#container div button')
+
+    numButtons.forEach((button) => {
+        let mouseDown
+        button.addEventListener('mouseup', () => {
+            mouseDown = false
+            button.style.borderLeft = '2px solid rgb(160, 160, 160)'
+            button.style.borderBottom = '2px solid rgb(103, 103, 103)'
+        })
+
+        button.addEventListener('mouseout', () => {
+            button.style.borderLeft = '2px solid rgb(160, 160, 160)'
+            button.style.borderBottom = '2px solid rgb(103, 103, 103)'
+        })
+        
+        button.addEventListener('mousedown', () => {
+            mouseDown = true
+            if (mouseDown) {
+                button.style.borderLeft = 'none'
+                button.style.borderBottom = 'none'
+            }
+        })
+
     })
 
-    button.addEventListener('mouseout', () => {
-        button.style.borderLeft = '2px solid rgb(160, 160, 160)'
-        button.style.borderBottom = '2px solid rgb(160, 160, 160)'
-    })
-    
-    button.addEventListener('mousedown', () => {
-        mouseDown = true
-        if (mouseDown) {
-            button.style.borderLeft = 'none'
-            button.style.borderBottom = 'none'
-        }
-    })
+
 
 })
-
